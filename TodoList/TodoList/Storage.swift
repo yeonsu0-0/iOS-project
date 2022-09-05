3821dbd765d6754945a56544d50cfb9459a84ac7
yeonsu <drawingbonny@gmail.com>
2022-09-05 21:58:28 +0900

    feat:Update Storage

--- TodoList/TodoList/Storage.swift
+++ TodoList/TodoList/Storage.swift
@@ -69,6 +69,85 @@ public class Storage {
             print("---> Failed to store msg: \(error.localizedDescription)")
         }
     }
+    
+    // TODO: 파일은 Data 타입형태로 읽을수 있음
+    // TODO: Data 타입은 Codable decode 가능
+    
+    static func retrive<T: Decodable>(_ fileName: String, from directory: Directory, as type: T.Type) -> T? {
+        let url = directory.url.appendingPathComponent(fileName, isDirectory: false)
+        guard FileManager.default.fileExists(atPath: url.path) else { return nil }
+        guard let data = FileManager.default.contents(atPath: url.path) else { return nil }
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let model = try decoder.decode(type, from: data)
+            return model
+        } catch let error {
+            print("---> Failed to decode msg: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    static func remove(_ fileName: String, from directory: Directory) {
+        let url = directory.url.appendingPathComponent(fileName, isDirectory: false)
+        guard FileManager.default.fileExists(atPath: url.path) else { return }
+        
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch let error {
+            print("---> Failed to remove msg: \(error.localizedDescription)")
+        }
+    }
+    
+    static func clear(_ directory: Directory) {
+        let url = directory.url
+        do {
+            let contents = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
+            for content in contents {
+                try FileManager.default.removeItem(at: content)
+            }
+        } catch {
+            print("---> Failed to clear directory ms: \(error.localizedDescription)")
+        }
+    }
 }
 
+// MARK: TEST 용
+extension Storage {
+    static func saveTodo(_ obj: Todo, fileName: String) {
+        let url = Directory.documents.url.appendingPathComponent(fileName, isDirectory: false)
+        print("---> [TEST] save to here: \(url)")
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+         
+        do {
+            let data = try encoder.encode(obj)
+            if FileManager.default.fileExists(atPath: url.path) {
+                try FileManager.default.removeItem(at: url)
+            }
+            FileManager.default.createFile(atPath: url.path, contents: data, attributes: nil)
+        } catch let error {
+            print("---> Failed to store msg: \(error.localizedDescription)")
+        }
+    }
+
+    
+    
+    static func restoreTodo(_ fileName: String) -> Todo? {
+        let url = Directory.documents.url.appendingPathComponent(fileName, isDirectory: false)
+        guard FileManager.default.fileExists(atPath: url.path) else { return nil }
+        guard let data = FileManager.default.contents(atPath: url.path) else { return nil }
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let model = try decoder.decode(Todo.self, from: data)
+            return model
+        } catch let error {
+            print("---> Failed to decode msg: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
 
