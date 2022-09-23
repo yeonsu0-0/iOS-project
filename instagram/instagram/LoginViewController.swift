4034ef0f1866222de8ed03d34a94910d96cb0033
yeonsu <drawingbonny@gmail.com>
2022-09-23 23:36:48 +0900

    feat: Add registerViewController

--- instagram/instagram/LoginViewController.swift
+++ instagram/instagram/LoginViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // 입력받은 값을 저장할 멤버 변수
     var email = String()
     var password = String()
     
@@ -20,7 +21,7 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func emailTextFieldEditingChanged(_ sender: UITextField) {
-        let text = sender.text ?? " "
+        let text = sender.text ?? " "   //옵셔널
         self.email = text
         print(text)
     }
@@ -31,5 +32,22 @@ class LoginViewController: UIViewController {
         self.password = text
     }
     
+    @IBAction func loginButtonDidTap(_ sender: UIButton) {
+    }
     
+    
+    @IBAction func registerButtonDidTap(_ sender: UIButton) {
+        
+        // 화면 전환 로직
+        // 스토리보드 생성 -> 뷰컨트롤러 생성 -> 화면 전환 메소드 이용해서 화면 전환
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let registerViewController = storyboard.instantiateViewController(withIdentifier: "RegisterVC") as! RegisterViewController
+        
+        // self.present(registerViewController, animated: true, completion: nil)
+        
+        // navigation controller push (embeded in 해서 사용)
+        self.navigationController?.pushViewController(registerViewController, animated: true)
+    }
 }
