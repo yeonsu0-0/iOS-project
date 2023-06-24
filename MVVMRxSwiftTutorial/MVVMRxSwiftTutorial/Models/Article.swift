bd4366887ec5f2a60eb8ee8cb64b5f3b84642c66
yeonsu <drawingbonny@gmail.com>
2023-06-24 09:57:31 +0900

    feat: Add struct models to fetch data

---
+++ MVVMRxSwiftTutorial/MVVMRxSwiftTutorial/Models/Article.swift
@@ -0,0 +1,23 @@
+//
+//  Article.swift
+//  MVVMRxSwiftTutorial
+//
+//  Created by bonny kim on 2023/06/24.
+//
+
+import Foundation
+
+struct ArticleResponse:Codable {
+    let status: String
+    let totalResults: Int
+    let articles:[Article]
+}
+
+struct Article:Codable {
+    let author: String?
+    let rirle: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+}
